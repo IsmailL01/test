bd122241357252a50a97a1f6d214eb2ef6ed57ce
Ismail <hummetzadeismail@gmail.com>
2024-05-15 23:21:06 +0300

    commit

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,16 @@
 const About = () => {
-  return <div>about</div>;
+  return (
+    <div>
+      abouts
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam quisquam
+        fuga quibusdam fugit dolorem modi eaque unde laboriosam. Quam illo
+        provident nemo distinctio voluptate explicabo odio veniam, recusandae
+        aliquid tenetur.
+      </p>
+      <p>hello worlds</p>
+    </div>
+  );
 };
 
 export default About;
